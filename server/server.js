35e6f75f6a47ae5bf7da9ff3be86860294653da4
Kleber C Batista <klebercal@gmail.com>
2017-01-26 08:29:51 -0200

    Gree new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,12 +15,24 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+  // socket.emit from Admin text Welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  })
+  // socket.broadcast.emit from Admin text New user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined!',
+    createdAt: new Date().getTime()
+  })
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message)
 
     message.createdAt = new Date().getTime()
     io.emit('newMessage', message)
-
+    // socket.broadcast.emit('newMessage', message)
   })
 
   socket.on('disconnect', () => {
